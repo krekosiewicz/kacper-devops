e50a38809321dd6f611d60220cea0975dc8306f1
krekosiewicz <rekosiewicz.kacper@gmail.com>
2024-05-23 17:41:56 +0200

    improve args/env in dockerfile

--- postfix-email-redirect/setup.sh
+++ postfix-email-redirect/setup.sh
@@ -5,8 +5,7 @@ echo "${EMAIL_FROM} ${EMAIL_TO}" > /config/virtual
 
 # Update Postfix configuration to use virtual alias map
 postconf -e "virtual_alias_maps = hash:/etc/postfix/virtual"
-
-# Configure logging
+postconf -e "mydestination = \$myhostname, localhost, localhost.localdomain, $MAILNAME"
 postconf -e "maillog_file = /var/log/mail.log"
 
 # Copy the virtual file to Postfix directory
@@ -17,3 +16,6 @@ postmap /etc/postfix/virtual
 
 # Restart Postfix to apply changes
 service postfix restart
+
+# Keep the container running
+tail -f /var/log/mail.log
